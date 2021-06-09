8cbd3cac196e6604ec9846c86c4b624004e05bc3
saes087364 <saes087364@gmail.com>
2021-06-10 02:49:35 +0800

    主角動作與攝影機設置告一段落

---
+++ Twilight_Overdrive_2D/Assets/Script/CameraControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject[] objectPlayer;
+    public Transform player;
+
+    public Vector3 vCam;
+    public Vector3 vPlayer;
+
+    [Range(0, 100)]
+    public int speed = 5;
+
+    public Vector2 limitX = new Vector2(-430.00f, 490.00f);
+    public Vector2 limitY = new Vector2(-2.00f, 53.00f);
+
+    private void Start()
+    {
+        objectPlayer = GameObject.FindGameObjectsWithTag("Player");
+        player = objectPlayer[0].GetComponent<Transform>();
+    }
+
+    private void LateUpdate()
+    {
+        Track();
+    }
+
+    private void Track()
+    {
+        vCam = this.transform.position;
+        vPlayer = player.transform.position;
+        vPlayer.x += 0.0f;
+        vPlayer.y += 5.0f;
+
+        vCam = Vector3.Lerp(vCam, vPlayer, speed * Time.deltaTime);
+        vCam.x = Mathf.Clamp(vCam.x, limitX.x, limitX.y);
+        vCam.y = Mathf.Clamp(vCam.y, limitY.x, limitY.y);
+        vCam.z = -10;
+
+        this.transform.position = vCam;
+    }
+}
